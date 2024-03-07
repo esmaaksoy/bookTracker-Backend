8c0d3be950d70e3e0e445238fb5c89f724030fbe
esma <esma.ea.aksoy@gmail.com>
2024-03-07 12:51:01 +0300

    feat: added router

--- backend/controllers/book.controller.js
+++ backend/controllers/book.controller.js
@@ -1,2 +1,47 @@
 "use strict";
-require('../models/book.model')
+
+const Book = require("../models/book.model");
+
+module.exports = {
+  list: async (req, res) => {
+    const data = await Book.findAndCountAll();
+    res.status(200).send({
+      error: false,
+      result: data,
+    });
+  },
+  create: async (req, res) => {
+    const data = await Book.create(req.body);
+    res.status(201).send({
+      error: false,
+      result: data.dataValues,
+    });
+  },
+  read: async (req, res) => {
+    const data = await Book.findByPk(req.params.id);
+    res.status(200).send({
+      error: false,
+      result: data,
+    });
+  },
+  update: async (req, res) => {
+    const data = await Book.update(req.body, { where: { id: req.params.id } });
+    res.status(202).send({
+      error: false,
+      message: "Updated",
+      body: req.body,
+      result: data,
+    });
+  },
+  delete: async (req, res) => {
+    const data = await Book.destroy({ where: { id: req.params.id } });
+    if ( data > 0){
+        res.sendStatus(204)
+    }else{
+       res.status(404).send({
+        error: true,
+        result:data
+       }) 
+    }
+  },
+};
--- backend/routes/book.router.js
+++ backend/routes/book.router.js
@@ -1,6 +1,9 @@
 "use strict";
 
-require('../controllers/book.controller')
+const Book = require('../controllers/book.controller')
 const router = require("express").Router()
+
+router.route('/').get(Book.list).post(Book.create)
+router.route("/:id").get(Book.read).put(Book.update).patch(Book.update).delete(Book.delete)
 
 module.exports = router
