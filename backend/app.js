f5c2ea30ef96692aa08adde713bbc0bcedc202eb
esma <esma.ea.aksoy@gmail.com>
2024-03-07 12:03:33 +0300

    feat:added errorHandler

---
+++ backend/app.js
@@ -0,0 +1,15 @@
+"use strict";
+
+const express = require("express");
+const app = express();
+
+require("dotenv").config()
+const PORT = process.env.PORT || 8000;
+
+app.use(express.json())
+require('express-async-errors')
+
+app.use(require('./routes/book.router'))
+app.use(require('./errorHandler'))
+
+app.listen(PORT, () => console.log("Running " + PORT))
