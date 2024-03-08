7e5bbcd42917f0ffab65abae1be8f298eb1ae2e5
esma <esma.ea.aksoy@gmail.com>
2024-03-08 22:25:46 +0300

    deleted frontend

--- backend/models/book.model.js
+++ backend/models/book.model.js
@@ -1,7 +1,7 @@
 "use strict";
 
 const { Sequelize, DataTypes } = require("sequelize");
-const sequelize = new Sequelize("sqlite:./db.sqlite3");
+const sequelize = new Sequelize("postgres://default:bcPYpez0gF5u@ep-jolly-bread-a45fz81v.us-east-1.aws.neon.tech:5432/verceldb?sslmode=require");
 
 module.exports = sequelize.define("books", {
   title: { type: DataTypes.STRING, allowNull: false },
